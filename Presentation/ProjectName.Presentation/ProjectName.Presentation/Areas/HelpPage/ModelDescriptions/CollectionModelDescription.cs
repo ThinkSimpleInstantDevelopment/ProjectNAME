f2e84e5cb36676fd45679736fddfc400890038a9
edoardo <edoardo.vanzi@gmail.com>
2015-05-12 14:35:30 +0200

    Created the site layout

---
+++ Presentation/ProjectName.Presentation/ProjectName.Presentation/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ProjectName.Presentation.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Presentation/ProjectName.Presentation/ProjectName.Presentation/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProjectName.Presentation.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
