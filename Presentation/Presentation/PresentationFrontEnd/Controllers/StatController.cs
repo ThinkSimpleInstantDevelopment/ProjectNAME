c4caa5320389974d950f37a02013393f50fc9a1b
edoardo <edoardo.vanzi@gmail.com>
2015-05-20 10:26:35 +0200

    basic implementatio of static datas

---
+++ Presentation/Presentation/PresentationFrontEnd/Controllers/StatController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PresentationFrontEnd.Controllers
+{
+    public class StatController : ApiController
+    {
+        // GET: api/Stat
+        public IEnumerable<Stat> Get()
+        {
+            DataAccess data = new DataAccess();
+            return data.Stat();
+        }
+
+        // GET: api/Stat/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Stat
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Stat/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Stat/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
