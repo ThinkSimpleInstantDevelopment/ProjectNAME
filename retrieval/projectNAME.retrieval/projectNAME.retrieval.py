64e1e5b985aef214695eb166014e24531d656301
Giovanni Luisotto <giovanni.luisotto@gmail.com>
2015-05-29 12:42:14 +0200

    versione definitiva funzionante

--- retrieval/projectNAME.retrieval/projectNAME.retrieval.py
+++ retrieval/projectNAME.retrieval/projectNAME.retrieval.py
@@ -1,15 +1,28 @@
 # python standard library
 import base64
 import json
+import logging
 
 # 3rd party
 import requests
+from logentries import LogentriesHandler
+
+
+def logger():
+    log = logging.getLogger('getTweets')
+    log.setLevel(logging.INFO)
+    log.addHandler(LogentriesHandler('21e95b59-8391-4f27-bf6f-d81894860920'))
+    return log
+
+
 '''
 get the token to make requests to twitter from client id and secret
 '''
 def get_token(client_id, client_secret):
-    client_id = "Kbvd9oQNAF6cr9XULzoRu5b1n"
-    client_secret = "dNJOsK3gcOiJJmrVdA1jKAt2usjzauQwXpiQpPv9CJCpUfo67W"
+    #client_id = TWITTER_CLIENT_ID
+    #client_id = 'Kbvd9oQNAF6cr9XULzoRu5b1n'
+    #client_secret = TWITTER_CLIENT_SECRET
+    #client_secret = 'nWoujP4PSOlIN1TrIpQ09lKXIDAyiJToddEcWuh6UyEnE'
     credentials = '{}:{}'.format(client_id, client_secret)
     credentials_b64 = base64.b64encode(credentials.encode())
     resp = requests.post(
@@ -31,7 +44,6 @@ def get_token(client_id, client_secret):
 
 # errore 429 all'esaurimento delle richieste per i 15min
 def search_tweets(what, token):
-    #what = 'java'
     '''
     Returns:
       a list of tweets (dictionaries)
@@ -40,7 +52,7 @@ def search_tweets(what, token):
     resp = requests.get(
         url,
         headers={'Authorization': 'Bearer {}'.format(token)},
-        params={'q': what, 'count':'1'}
+        params={'q': what, 'count':'100'}
     )
     resp.raise_for_status()
     data = resp.json()
@@ -70,41 +82,11 @@ def save_tweets_on_jsonfile(tweets):
     json.dump(cleanedTweet, f_destination)
     f_destination.close()
 
-
-'''
-create a customized tweet from a row one
-'''
-def resize(tweet):
-    data = {}
-    data['id_str'] = tweet['id_str']
-    data['programming_language'] = str(prog_lang)
-    data['created_at'] = tweet['created_at']
-    data['text'] = tweet['text']
-    data['favorite_count'] = tweet['favorite_count']
-    data['lang'] = tweet['lang']
-    data['coordinates'] = tweet['coordinates']
-    data['retweet_count'] = tweet['retweet_count']
-    data['place'] = tweet['place']
-    data['geo'] = tweet['geo']
-    hashtags = tweet['entities']['hashtags']
-    hashtag_list = []
-    for hashtag in hashtags:
-        hashtag_list.append(hashtag['text'])
-        data['hashtags'] = hashtag_list
-    data['user_geo_Enabled'] = tweet['user']['geo_enabled']
-    data['user_friends_count'] = tweet['user']['friends_count']
-    data['user_lang'] = tweet['user']['lang']
-    data['user_location'] = tweet['user']['location']
-    data['user_time_zone'] = tweet['user']['time_zone']
-    data['user_followers_count'] = tweet['user']['followers_count']
-    json_data = bson.dumps(data)
-    return data
-
 '''
 create a list of customized tweets from raw tweets
 '''
 def resize_cicle(tweets):
-    json_data_list = []
+    data_list = []
     for tweet in tweets:
         data = {}
         data['id_str'] = tweet['id_str']
@@ -128,25 +110,14 @@ def resize_cicle(tweets):
         data['user_location'] = tweet['user']['location']
         data['user_time_zone'] = tweet['user']['time_zone']
         data['user_followers_count'] = tweet['user']['followers_count']
-        json_data = json.dumps(data)
-	json_data_list.append(json_data)
-    return json_data_list
-
-'''
-convert raw datas in bson
-'''
-def to_bson(datas):
-    from bson import BSON
-    bson_data_list = []
-    for data in datas:
-        bson_data = BSON.encode(data)
-        bson_data_list.append(bson_data)
-    return bson_data_list
+        #json_data = json.dumps(data)
+	data_list.append(data)
+    return data_list
 
 '''
 returns a list of strings from a file
 '''
-def get_prog_lang(file):
+def get_prog_lang():
     file = './programmingLanguages.md'
     reader = open(file, 'r').read()
     langList = reader.split(',')
@@ -164,28 +135,30 @@ def save_in_db(tweetList):
     collection = db['retrieval']
     posts = db.posts
     db.test.insert_many(tweetList)
-    print(db.test.count())
-    #curs = db.test.find({})
-    #print curs
-    #for record in curs:
-    #    print record
 
 if __name__ == '__main__':
     import os
     import sys
-    print('getting token...')
-    token = get_token(
-        '3187020155-vjTxFyiyS2I13fDb1pLn2sFBjuLu0DIfvsyTAHi','nWoujP4PSOlIN1TrIpQ09lKXIDAyiJToddEcWuh6UyEnE'
+    log = logger()
+    #print('getting token...')
+    log.info('getting token...')
+    token = get_token('Kbvd9oQNAF6cr9XULzoRu5b1n','dNJOsK3gcOiJJmrVdA1jKAt2usjzauQwXpiQpPv9CJCpUfo67W'
         #os.environ['TWITTER_APP_ID'],
         #os.environ['TWITTER_APP_SECRET']
     )
-    prog_lang_list = get_prog_lang('~Desktop\tweets\programmingLanguages.md')
+    prog_lang_list = get_prog_lang()
+    tweetTotal = 0
     for prog_lang in prog_lang_list:
-        print('getting' + prog_lang + 'tweets...')
+        #print('getting ' + prog_lang + ' tweets...')
+        log.info('getting ' + prog_lang + ' tweets...')
         tweets = search_tweets(prog_lang, token)
         custom_tweets = resize_cicle(tweets)
-        bson_tweets = to_bson(tweets)
-        print('saving'+prog_lang+'tweets...')
-        save_in_db(bson_tweets)
-        print(prog_lang + 'ok')
+        #print(' saving'+prog_lang+' tweets...')
+        log.info('saving '+prog_lang+' tweets...')
+	tweetNum = len(custom_tweets)
+	tweetTotal += tweetNum
+        save_in_db(custom_tweets)
+        print(prog_lang + ' ok')
+	log.info(''+ str(tweetNum) +' '+ prog_lang +  ' tweets added')
     print('done')
+    log.info('done, totally added '+ str(tweetTotal) +' tweets')
