faf775af41c62440a27ce57c1caba479c83de6a1
Giovanni Luisotto <giovanni.luisotto@gmail.com>
2015-06-07 17:34:07 +0200

    added dataAccess test

---
+++ Presentation/ProjectName.Presentation.DataAccess.NewTest/DataAccessTest/Stats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessTest
+{
+ 
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+    using MongoDB.Driver;
+ 
+    [BsonIgnoreExtraElements]
+    public class Stats : MongoEntity
+    {
+        [BsonElement("ProgrammingLanguage")]
+        public string ProgrammingLanguage { get; set; }
+
+        [BsonElement("Created_at")]
+        public string Created_at { get; set; }
+
+        [BsonElement("Text")]
+        public string Text { get; set; }
+
+        [BsonElement("Lang")]
+        public string Lang { get; set; }
+    }
+}
